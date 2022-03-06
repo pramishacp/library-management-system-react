e32b71f9d739915f7ddab0910cb4e74f252226c8
pramishacp <pramishacp123@gmail.com>
2022-03-06 19:54:35 +0530

    define paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Navigate, Routes } from "react-router-dom";
 
-development
+import Library from "./components/library";
+import NotFound from "./components/notFound";
+
+import "./App.css";
 
 function App() {
   return (
     <React.Fragment>
+      <Routes>
+        <Route path="/library" element={<Library />} />
+        <Route path="/not-found" element={<NotFound />} />
+        <Route exact path="/" element={<Navigate to="/library" />} />
+        <Route path="*" element={<Navigate to="/not-found" />} />
+      </Routes>
     </React.Fragment>
   );
 }
