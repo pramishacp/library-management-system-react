d7f2f0a9fc0172951af59adeef44a151b6f3ad2c
pramishacp <pramishacp123@gmail.com>
2022-03-07 11:23:24 +0530

    create borrows component and render view

--- src/components/borrows.jsx
+++ src/components/borrows.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import { findBorrowsByUserId } from "../services/borrowService";
 
-const Borrows = () => {
-  return (
-    <div>
-      <h1>Borrows</h1>
-    </div>
-  );
-};
+class Borrows extends Component {
+  state = {
+    borrows: [],
+  };
+
+  async componentDidMount() {
+    const { data: borrows } = await findBorrowsByUserId("ad72d41c-3e1d-4580-94e8-746031ef7eca");
+    this.setState({ borrows });
+  }
+
+  render() {
+    const { length } = this.state.borrows;
+    const { borrows } = this.state;
+
+    return (
+      <React.Fragment>
+        <h2>Borrows</h2>
+        <p>{length} books borrowed from the library</p>
+        {length === 0 ? null : (
+          <ul>
+            {borrows.map((borrow) => {
+              return (
+                <li key={borrow.id}>
+                  <h6>
+                    {borrow.bookName}
+                  </h6>
+                  <div>
+                    <button>Return</button>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </React.Fragment>
+    );
+  }
+}
 
 export default Borrows;
-
--- src/services/borrowService.js
+++ src/services/borrowService.js
@@ -3,3 +3,7 @@ import http from "./httpService";
 export function borrowBook(userId, bookId) {
   return http.post(`/borrows`, { userId, bookId });
 }
+
+export function findBorrowsByUserId(userId) {
+  return http.get("/borrows/" + userId);
+}
